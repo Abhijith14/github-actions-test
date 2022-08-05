48f6759581e0dd75e2b269aa7255d26e9bbf8fde
ABHIJITH UDAYAKUMAR <abhijithukzm@gmail.com>
2022-08-05 15:54:52 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 
 # Write-Overwrites
-file1 = open("logs.txt", "w")  # write mode
-file1.write("Tomorrow \n")
+file1 = open("logs.txt", "a")  # write mode
+file1.write("Tomorrow is ahahha\n")
 file1.close()
